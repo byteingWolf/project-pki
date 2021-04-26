0ec0457042354ca7d50d9e9fe1dad17b9578fd93
wolf4fun <wilczek4fun@gmail.com>
2021-04-26 02:39:19 +0200

    github authorization

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
+const superagent = require('superagent')
 const path = require('path');
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 5000;
 const { google } = require('googleapis');
 const OAuth2Data = require('./google_key.json');
 
@@ -17,7 +18,8 @@ var user_image = null;
 const GITHUB_CLIENT_ID = "925eceb3fdae85614e56";
 const GITHUB_SECRET_ID = "8394e9f2a8c4d511f03a44fdf2c171742f17d516"
 const GITHUB_REDIRECT_URL = "https://project-pki-test.herokuapp.com/callback"
-var oAuthGitHub = new google.auth.OAuth2(GITHUB_CLIENT_ID,GITHUB_SECRET_ID,GITHUB_REDIRECT_URL)
+
+// var oAuthGitHub = new google.auth.OAuth2(GITHUB_CLIENT_ID,GITHUB_SECRET_ID,GITHUB_REDIRECT_URL)
 var auth_servie = false;
 
 app.use(express.static(path.join(__dirname, 'public')))
@@ -75,45 +77,44 @@ app.get('/logout', (req, res) => {
     }
 });
 
-app.get('/logWithGitHub', (req, res) => {
-    if (!auth) {
-        const url = oAuthGitHub.generateAuthUrl({
-            access_type: 'offline',
-            scope: 'https://github.com/login/oauth/authorize'
-        });
-        res.redirect(url);
-    }else{
-        const oauth_v2 = google.oauth2({ auth: oAuthGitHub, version: 'v2' });
-        oauth_v2.userinfo.v2.me.get(function (err, result) {
-            if (err) {
-                console.log(err);
-            } else {
-                auth = result.data.name;
-                user_image = result.data.picture
-            }
-            auth_servie = "GitHub"
-            res.render('pages/index',{auth: auth,user_picture :user_image,auth_servie:auth_servie});
-        });
-    }
-});
+// callback
+superagent
+  .post('https://github.com/login/oauth/access_token')
+  .send({ client_id: CLIENT_ID,
+    cliend_secret: CLIENT_SECRET}) 
+  .set('accept', 'json')
+  .then(function(result){
+    const data = result.body
+    res.send(data)
+  });
+ 
+// promise with then/catch
+// superagent.post('https://github.com/login/oauth/access_token').then(console.log).catch(console.error);
+ 
+(async () => {
+  try {
+    const res = await superagent.post('/');
+    console.log(res);
+  } catch (err) {
+    console.error(err);
+  }
+})();
 
-app.get('/callback', function (req, res) {
-    const code = req.query.code
-    if (code) {
-        // Get an access token based on our OAuth code
-        oAuthGitHub.getToken(code, function (err, tokens) {
-            if (err) {
-                console.log('Error authenticating')
-                console.log(err);
-            } else {
-                console.log('Successfully authenticated');
-                oAuthGitHub.setCredentials(tokens);
-                auth = true;
-                res.redirect('/log')
-            }
-        });
-    }
-});
+
+// app.get('/logWithGitHub', (req, res) => {
+
+// });
+
+// app.get('/logWithGitHub/callback', function (req, res) {
+//     const code = req.query.code
+//     if (!code) {
+//         return res.send({
+//             succes: false,
+//             message: "Failed"
+//         })
+//     }
+// });
+
 
 app.get('/logoutGitHub', (req, res) => {
     if(auth)
