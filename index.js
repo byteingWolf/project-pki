77b532c2540a5dda72a3974bea25da554a4a8c01
wolf4fun <wilczek4fun@gmail.com>
2021-06-12 21:01:33 +0200

    testing without view engine

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const path = require('path');
 const PORT = process.env.PORT || 3000;
 const { google } = require('googleapis');
 const OAuth2Data = require('./google_key.json');
-const { Pool } = require('pg');
+const { Client } = require('pg');
+const { response } = require('express');
 const app = express()
 
 const CLIENT_ID = OAuth2Data.web.client_id;
@@ -15,10 +16,10 @@ var auth = false;
 var user_image = false;
 
 app.use(express.static(path.join(__dirname, 'public')))
-app.engine('pug', require('pug').__express)
+// app.engine('pug', require('pug').__express)
 app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'pug')
-app.get('/', (req, res) => res.render('pages/index', { auth: auth }))
+// app.set('view engine', 'pug')
+app.get('/', (req, res) => res.render('pages/index_with_table', { auth: auth }))
 app.get('/log', (req, res) => {
     if (!auth) {
         const url = oAuth2Client.generateAuthUrl({
@@ -28,12 +29,21 @@ app.get('/log', (req, res) => {
         res.redirect(url);
     } else {
         const oauth_v2 = google.oauth2({ auth: oAuth2Client, version: 'v2' });
-        oauth_v2.userinfo.v2.me.get(function (err, result) {
+        oauth_v2.userinfo.v2.me.get(async function (err, result) {
             if (err) {
                 console.log(err);
             } else {
                 auth = result.data.name;
                 user_image = result.data.picture
+            }
+            const user = await getUser(auth)
+            if (user.rows.length > 0) {
+                updateUserCounter(auth)
+                // response.send(`updated ${userName}`)
+            }
+            else {
+                addUser(auth)
+                // response.send(`added ${userName}`)
             }
             res.render('pages/index', { auth: auth, user_picture: user_image });
         });
@@ -66,23 +76,67 @@ app.get('/logout', (req, res) => {
     }
 });
 
-const getPool = () => new Pool({
-    connectionString: "postgres://qinzjcnqfmoyrs:6f227b6fc9409dedd626f7b54c8d576595a83c9bdffa3af103e948dd8d97281c@ec2-35-170-85-206.compute-1.amazonaws.com:5432/d4895c1g577mqb"
+const client = new Client({
+    connectionString: "postgres://qinzjcnqfmoyrs:6f227b6fc9409dedd626f7b54c8d576595a83c9bdffa3af103e948dd8d97281c@ec2-35-170-85-206.compute-1.amazonaws.com:5432/d4895c1g577mqb",
+    ssl: {
+        rejectUnauthorized: false
+    }
 })
 
-const client = getPool();
 client.connect();
 
 app.get('/getUsers', (request, response) => {
     console.log('Pobieram dane ...');
-    client.query('SELECT * FROM public."Users"', (error, res) => {
-        if (error) {
+    client.query('SELECT * FROM public."users"', (error, res) => {
+        if (error)
             throw error
-        }
         console.log('Dostałem ...');
         for (let row of res.rows) {
             console.log(JSON.stringify(row));
         }
     })
+    response.send('udalo sie pobrac')
 })
+
+async function updateUserCounter(userName) {
+    const timeStamp = Math.floor(Date.now() / 1000)
+    client.query(`UPDATE users SET counter = counter + 1,lastvisit = to_timestamp(${timeStamp}) where name = '${userName}' `, (error, res) => {
+        if (error) {
+            throw error
+        }
+        console.log(userName + " updated");
+    })
+}
+
+async function addUser(userName) {
+    const timeStamp = Math.floor(Date.now() / 1000)
+    const list = [userName, timeStamp, timeStamp]
+    return client.query(`INSERT INTO users (name,joined,lastvisit) VALUES ($1,to_timestamp($2),to_timestamp($3))`, list, (error, res) => {
+        if (error) {
+            throw error
+        }
+        console.log(userName + " addded");
+    })
+}
+
+async function getUser(userName) {
+    return client.query(`select * from users where name = '${userName}'`);
+}
+
+// app.get('/addUser', async (request, response) => {
+//     const userName = 'kotek1'
+//     const user = await getUser(userName)
+//     if (user.rows.length > 0) {
+//         console.log("istnieje")
+//         updateUserCounter(userName)
+//         response.send(`updated ${userName}`)
+//     }
+//     else {
+//         console.log("nie istnieje")
+//         addUser(userName)
+//         response.send(`added ${userName}`)
+//     }
+// })
+
+
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
